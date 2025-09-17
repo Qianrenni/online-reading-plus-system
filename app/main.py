49d516fdaf131aae38a854f0bf4aafe2fd373be6
qianrenni <15133897+Qianrenni@user.noreply.gitee.com>
2025-09-18 03:35:48 +0800

    feat:初版

--- app/main.py
+++ app/main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
+from fastapi.openapi.docs import get_swagger_ui_html, get_swagger_ui_oauth2_redirect_html
 
-app = FastAPI()
+app = FastAPI(docs_url=None)
 
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",  # ← 国内能访问
+        swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",      # ← 国内能访问
+    )
 
+@app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
+async def swagger_ui_redirect():
+    return get_swagger_ui_oauth2_redirect_html()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
